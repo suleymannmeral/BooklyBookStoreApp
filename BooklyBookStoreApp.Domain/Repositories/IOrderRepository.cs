e0cb26034f1a7f192b68be0cacd52e19de859c0c
Suleyman Meral <mrlslymn02@gmail.com>
2025-08-07 14:03:29 +0300

    test

--- BooklyBookStoreApp.Domain/Repositories/IOrderRepository.cs
+++ BooklyBookStoreApp.Domain/Repositories/IOrderRepository.cs
@@ -6,7 +6,7 @@ namespace BooklyBookStoreApp.Domain.Repositories;
 public interface IOrderRepository: IRepositoryBase<Order>
 {
     IQueryable<Order> GetAllOrdersByUserId(bool trackChanges);
-    IQueryable<Order> GetOneOrderByIdAndUserId(int id, bool trackChanges);
+    IQueryable<Order> GetOneOrderByIdAndUserId(int id,string userid, bool trackChanges);
     void CreateOrder(Order order);
     void UpdateOrder(Order order);
     void DeleteOrder(Order order);
