ad15894548307a49d410dfd0b4459171553770e1
Suleyman Meral <mrlslymn02@gmail.com>
2025-08-07 13:23:06 +0300

    feat: Add IOrderRepository

---
+++ BooklyBookStoreApp.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using BooklyBookStoreApp.Domain.Entitites;
+
+
+namespace BooklyBookStoreApp.Domain.Repositories;
+
+public interface IOrderRepository: IRepositoryBase<Order>
+{
+    IQueryable<Order> GetAllOrdersByUserId(bool trackChanges);
+    IQueryable<Order> GetOneOrderByIdAndUserId(int id, bool trackChanges);
+    void CreateOrder(Order order);
+    void UpdateOrder(Order order);
+    void DeleteOrder(Order order);
+}
